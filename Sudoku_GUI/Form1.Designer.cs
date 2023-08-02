5cc54a6ffe19c0c13fa60c6ace1f6331efa64b0d
Cristian Acalo <cjacalo@espe.edu.ec>
2023-08-02 12:46:53 -0500

    Creating a basic board with random valid numbers

--- Sudoku_GUI/Form1.Designer.cs
+++ Sudoku_GUI/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label_principal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 82);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(484, 447);
+            this.panel1.TabIndex = 0;
+            // 
+            // label_principal
+            // 
+            this.label_principal.AutoSize = true;
+            this.label_principal.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_principal.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label_principal.Location = new System.Drawing.Point(165, 20);
+            this.label_principal.Name = "label_principal";
+            this.label_principal.Size = new System.Drawing.Size(170, 39);
+            this.label_principal.TabIndex = 1;
+            this.label_principal.Text = "SUDOKU";
+            this.label_principal.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label_principal.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(508, 542);
+            this.Controls.Add(this.label_principal);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label_principal;
     }
 }
 
--- Sudoku_GUI/Form1.cs
+++ Sudoku_GUI/Form1.cs
@@ -15,6 +15,174 @@ namespace Sudoku_GUI
         public Form1()
         {
             InitializeComponent();
+            this.BackColor = System.Drawing.Color.White;
+            createCells();
+            startNewGame();
+        }
+
+        SudokuCell[,] cells = new SudokuCell[9, 9];
+
+        private void createCells()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    // Create 81 cells for with styles and locations based on the index
+                    cells[i, j] = new SudokuCell();
+                    cells[i, j].Font = new Font(SystemFonts.DefaultFont.FontFamily, 20);
+                    cells[i, j].Size = new Size(40, 40);
+                    cells[i, j].ForeColor = SystemColors.ControlDarkDark;
+                    cells[i, j].Location = new Point(i * 40, j * 40);
+                    cells[i, j].BackColor = ((i / 3) + (j / 3)) % 2 == 0 ? Color.FromArgb(255, 255, 255) : Color.LightCyan;
+                    cells[i, j].FlatStyle = FlatStyle.Flat;
+                    cells[i, j].FlatAppearance.BorderColor = Color.Gray;
+                    cells[i, j].X = i;
+                    cells[i, j].Y = j;
+
+                    // Assign key press event for each cells
+                    cells[i, j].KeyPress += cell_keyPressed;
+
+                    panel1.Controls.Add(cells[i, j]);
+                }
+            }
+        }
+
+        private void cell_keyPressed(object sender, KeyPressEventArgs e)
+        {
+            var cell = sender as SudokuCell;
+
+            // Do nothing if the cell is locked
+            if (cell.IsLocked)
+                return;
+
+            int value;
+
+            // Add the pressed key value in the cell only if it is a number
+            if (int.TryParse(e.KeyChar.ToString(), out value))
+            {
+                // Clear the cell value if pressed key is zero
+                if (value == 0)
+                    cell.Clear();
+                else
+                    cell.Text = value.ToString();
+
+                cell.ForeColor = SystemColors.ControlDarkDark;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void startNewGame()
+        {
+            loadValues();
+            //Show values of 45 cells as hint
+            showRandomValuesHints(45);
+        }
+
+        private void loadValues()
+        {
+            // Clear the values in each cells
+            foreach (var cell in cells)
+            {
+                cell.Value = 0;
+                cell.Clear();
+            }
+
+            // This method will be called recursively 
+            // until it finds suitable values for each cells
+            findValueForNextCell(0, -1);
+        }
+
+        Random random = new Random();
+
+        private bool findValueForNextCell(int i, int j)
+        {
+            // Increment the i and j values to move to the next cell
+            // and if the columsn ends move to the next row
+            if (++j > 8)
+            {
+                j = 0;
+
+                // Exit if the line ends
+                if (++i > 8)
+                    return true;
+            }
+
+            var value = 0;
+            var numsLeft = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            // Find a random and valid number for the cell and go to the next cell 
+            // and check if it can be allocated with another random and valid number
+            do
+            {
+                // If there is not numbers left in the list to try next, 
+                // return to the previous cell and allocate it with a different number
+                if (numsLeft.Count < 1)
+                {
+                    cells[i, j].Value = 0;
+                    return false;
+                }
+
+                // Take a random number from the numbers left in the list
+                value = numsLeft[random.Next(0, numsLeft.Count)];
+                cells[i, j].Value = value;
+
+                // Remove the allocated value from the list
+                numsLeft.Remove(value);
+            }
+            while (!isValidNumber(value, i, j) || !findValueForNextCell(i, j));
+
+            // TDO: Remove this line after testing
+            //cells[i, j].Text = value.ToString();
+
+            return true;
+        }
+
+        private bool isValidNumber(int value, int x, int y)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                // Check all the cells in vertical direction
+                if (i != y && cells[x, i].Value == value)
+                    return false;
+
+                // Check all the cells in horizontal direction
+                if (i != x && cells[i, y].Value == value)
+                    return false;
+            }
+
+            // Check all the cells in the specific block
+            for (int i = x - (x % 3); i < x - (x % 3) + 3; i++)
+            {
+                for (int j = y - (y % 3); j < y - (y % 3) + 3; j++)
+                {
+                    if (i != x && j != y && cells[i, j].Value == value)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void showRandomValuesHints(int hintsCount)
+        {
+            // Show value in random cells
+            // The hints count is based on the level player choose
+            for (int i = 0; i < hintsCount; i++)
+            {
+                var rX = random.Next(9);
+                var rY = random.Next(9);
+
+                // Style the hint cells differently and
+                // lock the cell so that player can't edit the value
+                cells[rX, rY].Text = cells[rX, rY].Value.ToString();
+                cells[rX, rY].ForeColor = Color.Black;
+                cells[rX, rY].IsLocked = true;
+            }
         }
     }
 }
